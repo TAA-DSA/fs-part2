ac239ce7658d09d37295ec534349d1d5cfb931fb
Tasdeed <ayonaziz@gmail.com>
2024-02-02 16:13:40 -0500

    ex 2.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-const App = () => {
-  console.log("Hello from App");
+const App = ({ course }) => {
+  const exerciseArr = course.parts.map((element) => {
+    return element.exercises;
+  });
+  console.log(exerciseArr);
+
+  const total = exerciseArr.reduce((acc, curr) => acc + curr);
+  console.log(total);
+
   return (
     <div>
-      <p>Hello World</p>
+      <h1>{course.name}</h1>
+      {course.parts.map((items) => (
+        <p key={items.id}>
+          {items.name} {items.exercises}
+        </p>
+      ))}
+      <h2>total of {total} exercises</h2>
     </div>
   );
 };
