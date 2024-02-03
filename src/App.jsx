01f4a6f980ed6ce42043d3e1710b4ed92958526e
Tasdeed <ayonaziz@gmail.com>
2024-02-02 21:46:25 -0500

    ex 2.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 const App = ({ course }) => {
-  const exerciseArr = course.parts.map((element) => {
-    return element.exercises;
-  });
-  console.log(exerciseArr);
+  console.log(course);
 
-  const total = exerciseArr.reduce((acc, curr) => acc + curr);
-  console.log(total);
+  const exerciseFunc = (index) => {
+    const exerciseArr = course[index].parts.map((element) => element.exercises);
+    return exerciseArr.reduce((acc, curr) => acc + curr);
+  };
+
+  // const total = exerciseArr.reduce((acc, curr) => acc + curr);
+  // console.log(total);
 
   return (
     <div>
-      <h1>{course.name}</h1>
-      {course.parts.map((items) => (
+      <h1>Web development Curriculum</h1>
+      <h2>{course[0].name}</h2>
+      {course[0].parts.map((items) => (
         <p key={items.id}>
           {items.name} {items.exercises}
         </p>
       ))}
-      <h2>total of {total} exercises</h2>
+      <h2>total of {exerciseFunc(0)} exercises</h2>
+      <h1>{course[1].name}</h1>
+      {course[1].parts.map((items) => (
+        <p key={items.id}>
+          {items.name} {items.exercises}
+        </p>
+      ))}
+      <h2>total of {exerciseFunc(1)} exercises</h2>
     </div>
   );
 };
